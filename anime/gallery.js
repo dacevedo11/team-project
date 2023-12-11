c71f4ba63063977a30e971a359f6da42cf6ceadf
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-12-10 20:32:13 -0800

    l

--- anime/gallery.js
+++ anime/gallery.js
@@ -1,54 +1,22 @@
-const baseURL = "https://api.jikan.moe/v4/anime";
-let searchQuery = ""; // Replace this with the search query entered by the user
+const baseURL = "https://api.jikan.moe/v4/top/anime"; // Using the top anime endpoint
 
-$("#searchButton").click(function () {
-    // Get the search query from an input field or any other source
-    searchQuery = $("#searchInput").val();
-
-    // Make the request with the search query
-    const settings = {
-        url: `${baseURL}?q=${searchQuery}`,
-        method: "GET",
-        dataType: "json",
-        success: searchSuccess,
-        error: ajaxError
-    };
-
-    console.log("New URL:", settings.url);
-
-    $.ajax(settings);
-});
+function loadGallery() {
+    fetch('https://api.jikan.moe/v4/top/anime')
+        .then(response => response.json())
+        .then(data => searchSuccess(data))
+        .catch(error => ajaxError(error));
+}
 
 function searchSuccess(data) {
-    // Check if there are search results
-    if (data.data && data.data.length > 0) {
-        // Clear previous results
-        $("#output").empty();
-
-        // Create a container for the gallery
-        const gallery = $('<div class="gallery"></div>');
-
-        // Loop through all the results
-        data.data.forEach((result, index) => {
-            // Append only the image to the gallery container
-            gallery.append(`
-                <div class="gallery-item">
-                    <img src="${result.images.jpg.large_image_url}" alt="${result.title}">
-                </div>
-            `);
-        });
-
-        // Append the gallery container to the #output div
-        $("#output").append(gallery);
-    } else {
-        $("#output").html("<p>No results found</p>");
-    }
+    // Rest of your code
 }
 
-function ajaxError(request, status, error) {
-    console.error("Oops:", request, status, error);
+function ajaxError(error) {
+    console.error("Error fetching data:", error);
 }
 
-$(document).ready(function () {
-    // Code to be executed when the document is ready
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadGallery();
 });
+
+
